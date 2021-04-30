6982e7ea3004c9ac16e7d6040621534ddfc5296b
Jean-Dominique Richard <07sian07@gmail.com>
2021-04-30 19:01:34 +0200

    i: font variabilised

--- CookieClicker/cookie_clicker.py
+++ CookieClicker/cookie_clicker.py
@@ -11,9 +11,11 @@ wn.bgcolor("black")
 
 #"TEXT"
 #"(fontname, fontsize, fonttype)"
-_font=("Arial",8,"normal")
+_fontInfo=("Arial",8,"normal")
+_fontNotice=("Serif",10,"bold")
 _textAlign="center"
 _textPosition=(0,200)
+_noticePosition=(0,220)
 _textColor="white"
 
 wn.register_shape("cookie.gif")
@@ -29,23 +31,23 @@ pen.hideturtle()
 pen.color(_textColor)
 pen.penup()
 pen.goto(_textPosition)
-pen.write(arg=f"Clicks: {clicks}",align=_textAlign, font=_font)
+pen.write(arg=f"Clicks: {clicks}",align=_textAlign, font=_fontInfo)
 
 def clicked(x, y):
     global clicks
     clicks += 1
     pen.clear()
     #pen.write(arg=f"Clicks: {clicks}", align=_textAlign, font=_font)"
-    pen.write(arg=f"Clicks: {clicks}",align=_textAlign)
+    pen.write(arg=f"Clicks: {clicks}",align=_textAlign,font=_fontInfo)
     noticed()
     pen.goto(_textPosition)
 
 
 def noticed():
     global clicks
-    pen.goto(0,210)
+    pen.goto(_noticePosition)
     if (clicks%10==0) :
-        pen.write(arg=f"deja {clicks} clicks",align=_textAlign)
+        pen.write(arg=f"deja {clicks} clicks",align=_textAlign,font=_fontNotice)
     
 cookie.onclick(clicked)
 
