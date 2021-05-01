f4c8b3027ce140d2e77d27cb88fbeabe60445112
Jean-Dominique Richard <07sian07@gmail.com>
2021-05-01 14:21:49 +0200

    :i prepare presentation

--- CookieClicker/cookie_clicker.py
+++ CookieClicker/cookie_clicker.py
@@ -2,30 +2,36 @@
 # Simple Cookie Clicker Clone
 # Python 3.x Compatible
 # Windows, MacOSX, and Linux Compatible
-# by @Fenix and @Sian
+
+#import de turtle pour la fenetre et le crayon du jeu 
 import turtle
 
-wn = turtle.Screen()
-wn.title("Cookie Clicker by @Fenix")
-wn.bgcolor("black")
+#je crée la fenetre avec turtle 
+screen = turtle.Screen()
+#je met le titre et la couleur de font
+screen.title("Cookie Clicker")
+screen.bgcolor("black")
 
-#"TEXT"
-#"(fontname, fontsize, fonttype)"
+#variables
+#tuples info text :  font + position
 _infoFont=("Arial",8,"normal")
 _infoPosition=(0,200)
+#tuples notice text :  font + position
 _noticePosition=(0,220)
 _noticeFont=("Serif",10,"bold")
+#text
 _textAlign="center"
 _textColor="white"
+#clicks
+clicks = 0
 
-wn.register_shape("cookie.gif")
-
+#j'enregistre et je met l'image de cookie
+screen.register_shape("cookie.gif")
 cookie = turtle.Turtle()
 cookie.shape("cookie.gif")
 cookie.speed(0)
 
-clicks = 0
-
+#j'écrie le text qui affiche les click
 pen = turtle.Turtle()
 pen.hideturtle()
 pen.color(_textColor)
@@ -33,21 +39,25 @@ pen.penup()
 pen.goto(_infoPosition)
 pen.write(arg=f"Clicks: {clicks}",align=_textAlign, font=_infoFont)
 
+#je définie la méthode que j'effectue au click
 def clicked(x, y):
     global clicks
     clicks += 1
     pen.clear()
     pen.write(arg=f"Clicks: {clicks}",align=_textAlign,font=_infoFont)
-    noticed()
+    noticeGamer()
     pen.goto(_infoPosition)
 
-
-def noticed():
+#je définie la méthode qui informe le joueur 
+def noticeGamer():
     global clicks
     pen.goto(_noticePosition)
+    #tous les 10 click j'informe le joueur
     if (clicks%10==0) :
         pen.write(arg=f"deja {clicks} clicks",align=_textAlign,font=_noticeFont)
-    
+
+#j'utilise ma méthode (clicked) lorsque le cookie est cliqué 
 cookie.onclick(clicked)
 
-wn.mainloop()
+#je lance la boucle principal du jeu
+screen.mainloop()
